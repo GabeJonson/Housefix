d58231583b51e6b19211905b99de13c46d113ae2
Gabe Jonson <sky3107@gmail.com>
2017-06-28 11:00:57 +0300

    Header done!

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-
+  HouseFix.init();
 });
+
+var HouseFix = (function() {
+  return{
+    init: function() {
+      this.sandwitch();
+    },
+
+    sandwitch: function() {
+      $('.sandwitch').click(function() {
+        $(this).toggleClass('active');
+
+        $('.nav').slideToggle('fast');
+      });
+    }
+  }
+})();
